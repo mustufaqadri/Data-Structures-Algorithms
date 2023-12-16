8101370518d9bc1804d785a21636f20028443892
Mustufa Qadri <smustufaqadri@gmail.com>
2023-12-16 13:16:17 +0500

    Fibonacci Series

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,8 +3,7 @@ public class Fibonacci {
         Problem Link: https://leetcode.com/problems/fibonacci-number/description/
     */
     public static int solve(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
+        if (n <= 1) return n;
 
         int t1 = 0, t2 = 1, tn = 0;
         int index = 1;
