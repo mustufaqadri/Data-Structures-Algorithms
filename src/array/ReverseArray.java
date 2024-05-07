582ca0f12c0a6943abf579de8ed462b6095c535d
Mustufa Qadri <mustufa.qadri@sleepnumber.com>
2024-05-07 21:17:28 +0500

    Verifying setup

--- src/array/ReverseArray.java
+++ src/array/ReverseArray.java
@@ -5,7 +5,7 @@ public class ReverseArray {
         This program will reverse the given array.
     */
     public static void solve(int[] arr) {
-        int start = 0, end = arr.length - 1;
+        int start = 0, end = arr.length - 2;
         int temp;
 
         while (start < end) {
